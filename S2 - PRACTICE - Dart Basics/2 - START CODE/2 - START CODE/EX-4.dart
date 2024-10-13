726bdea8294b1fd5ebf2905d8abbfd7b89273bab
Ritsumeotakiri <141367098+Ritsumeotakiri@users.noreply.github.com>
2024-10-13 21:47:29 +0700

    Update EX-4.dart

--- S2 - PRACTICE - Dart Basics/2 - START CODE/2 - START CODE/EX-4.dart
+++ S2 - PRACTICE - Dart Basics/2 - START CODE/2 - START CODE/EX-4.dart
@@ -11,5 +11,20 @@ void main() {
 
   // Your code
   double total = 0;
-  const orderdetail = [];
+  List<String> invalidPizzas = [];
+
+  for (var pizza in order) {
+    if (pizzaPrices.containsKey(pizza)) {
+      total += pizzaPrices[pizza]!;// if we find the pizza we will add the price to the total
+    } else {
+      invalidPizzas.add(pizza); // if there are no name it will add the pizza to the dont have list
+    }
+  }
+  print('Total: \$${total}');
+
+  if (invalidPizzas.isNotEmpty) {
+    for (var pizza in invalidPizzas) {
+      print('$pizza is not on the menu');
+    }
+  }
 }
