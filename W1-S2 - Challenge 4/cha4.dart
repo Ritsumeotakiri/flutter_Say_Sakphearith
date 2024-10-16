5edb4afb185daa38ea62ebc37a2cc9a9fadc9c87
Ritsumeotakiri <141367098+Ritsumeotakiri@users.noreply.github.com>
2024-10-17 00:21:41 +0700

    Update cha4.dart
    add comment to under code

--- W1-S2 - Challenge 4/cha4.dart
+++ W1-S2 - Challenge 4/cha4.dart
@@ -1,15 +1,17 @@
 class Distance {
-  final double _meters;
+  final double _meters; // make immutability by using final
 
+  //create construnctor for cms,meter, and kms it can convert to other unit
   Distance.cms(double cms) : _meters = cms / 100;
   Distance.meters(double meters) : _meters = meters;
   Distance.kms(double kms) : _meters = kms * 100;
 
+  //use getter  to convert to other unit
   double get cms => _meters * 100;
   double get meter => _meters;
   double get kms => _meters / 1000;
 
-  @override
+  //operation to plus the unit that convert and than can convert to the other unit that command
   Distance operator + (Distance other) {
     return Distance.meters(this._meters + other._meters);
   }
